2a7d0ca7084c3aa41e673e623947a2f8374eb4b2
Shatakshi Singh <s.shatakshi2103@gmail.com>
2021-06-29 22:15:28 +0530

    Presentation Splash Screen

--- lib/app/auth/presentation/splash-screen/splash-screen-controller.dart
+++ lib/app/auth/presentation/splash-screen/splash-screen-controller.dart
@@ -1,1 +1,53 @@
+import 'package:clean_architecture_todo/app/auth/presentation/splash-screen/splash-screen-presenter.dart';
+import 'package:clean_architecture_todo/app/auth/presentation/splash-screen/splash-screen-state-machine.dart';
+import 'package:clean_architecture_todo/app/navigation-service.dart';
+import 'package:clean_architecture_todo/core/presentation/observer.dart';
+import 'package:clean_architecture_todo/injection-container.dart';
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
 
+class SplashScreenController extends Controller {
+  final SplashScreenPresenter _presenter;
+  final SplashScreenStateMachine _stateMachine;
+  final NavigationService _navigationService;
+
+  SplashScreenController()
+      : _presenter = serviceLocator<SplashScreenPresenter>(),
+        _stateMachine = new SplashScreenStateMachine(),
+        _navigationService = serviceLocator<NavigationService>(),
+        super();
+
+  @override
+  void initListeners() {
+    // TODO: implement initListeners
+  }
+
+  @override
+  void onInitState() {
+    super.onInitState();
+  }
+
+  @override
+  void onDisposed() {
+    _presenter.dispose();
+    super.onDisposed(); //cleaning
+  }
+
+  SplashScreenState getCurrentState() {
+    return _stateMachine.getCurrentState();
+  }
+
+  void userSignInCheckStatus() {
+    _presenter.checkUserSignIn(new UseCaseObserver(() {}, (error) {
+      _navigationService.navigateTo(NavigationService.signInPageRoute,
+          shouldReplace: true);
+    }, onNextFunc: (bool signInStatus) {
+      if (signInStatus) {
+        _navigationService.navigateTo(NavigationService.homePageRoute,
+            shouldReplace: true);
+      } else {
+        _navigationService.navigateTo(NavigationService.signInPageRoute,
+            shouldReplace: true);
+      }
+    }));
+  }
+}
--- lib/app/auth/presentation/splash-screen/splash-screen-state-machine.dart
+++ lib/app/auth/presentation/splash-screen/splash-screen-state-machine.dart
@@ -1,1 +1,24 @@
- //SplashScreenInitializationState ie InitState
+//SplashScreenInitializationState instead of InitState
+
+//for fetching from firebase Initialization State is used
+
+import 'package:clean_architecture_todo/core/presentation/state-machine.dart';
+
+class SplashScreenStateMachine
+    extends StateMachine<SplashScreenState, SplashScreenEvent> {
+  SplashScreenStateMachine() : super(SplashScreenInitializationState());
+  @override
+  SplashScreenState getStateOnEvent(SplashScreenEvent event) {
+    final eventType = event.runtimeType;
+    SplashScreenState newState = getCurrentState();
+    switch (eventType) {
+    }
+    return newState;
+  }
+}
+
+abstract class SplashScreenState {}
+
+class SplashScreenInitializationState extends SplashScreenState {}
+
+abstract class SplashScreenEvent {}
