87c089783e656260ea00d34a0e566e50471d21c5
Shatakshi Singh <s.shatakshi2103@gmail.com>
2021-07-13 00:01:29 +0530

    Auth page design

--- lib/app/auth/presentation/sign-up/view/widgets/sign-up-content-body.dart
+++ lib/app/auth/presentation/sign-up/view/widgets/sign-up-content-body.dart
@@ -20,37 +20,71 @@ class SignUpContentBody extends StatefulWidget {
 class _SignUpContentBodyState extends State<SignUpContentBody> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(height: 100.0),
-        Text('Sign Up'),
-        TextFormField(
-          controller: widget.emailTextController,
-          decoration:
-              InputDecoration(labelText: "Email", hintText: "abc@gmail.com"),
-        ),
-        TextFormField(
-          controller: widget.passwordTextController,
-          decoration: InputDecoration(labelText: "Password"),
-        ),
-        TextButton(
-          onPressed: () {
-            if (widget.emailTextController.text.isNotEmpty &&
-                widget.passwordTextController.text.isNotEmpty) {
-              widget.controller.userSignUp(
-                  email: widget.emailTextController.text,
-                  password: widget.passwordTextController.text);
-            }
-          },
-          child: Text("Sign Up"),
-        ),
-        TextButton(
-          onPressed: () {
-            widget.controller.navigateToSignIn();
-          },
-          child: Text("Go to Sign In"),
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          SizedBox(height: 100.0),
+          Text(
+            'Sign Up',
+            style: TextStyle(fontSize: 20),
+          ),
+          SizedBox(height: 17.0),
+          TextFormField(
+            controller: widget.emailTextController,
+            decoration: InputDecoration(
+              labelText: "Email",
+              prefixIcon: const Icon(
+                Icons.email,
+                color: Colors.grey,
+              ),
+              hintText: "abc@gmail.com",
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.black, width: 1.5),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+          ),
+          SizedBox(height: 17.0),
+          TextFormField(
+            controller: widget.passwordTextController,
+            decoration: InputDecoration(
+              labelText: "Password",
+              prefixIcon: const Icon(
+                Icons.lock,
+                color: Colors.grey,
+              ),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderSide: const BorderSide(color: Colors.black, width: 1.5),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+            ),
+            obscureText: true,
+          ),
+          TextButton(
+            onPressed: () {
+              if (widget.emailTextController.text.isNotEmpty &&
+                  widget.passwordTextController.text.isNotEmpty) {
+                widget.controller.userSignUp(
+                    email: widget.emailTextController.text,
+                    password: widget.passwordTextController.text);
+              }
+            },
+            child: Text("Sign Up"),
+          ),
+          TextButton(
+            onPressed: () {
+              widget.controller.navigateToSignIn();
+            },
+            child: Text("Go to Sign In"),
+          ),
+        ],
+      ),
     );
   }
 }
