3bcfb78995f25bc143dca513e6fa2eda3b66339a
Shatakshi Singh <s.shatakshi2103@gmail.com>
2021-07-28 23:38:02 +0530

    todo-impl

--- lib/app/todo/data/data-repository-impl.dart
+++ lib/app/todo/data/data-repository-impl.dart
@@ -1,6 +1,18 @@
 import 'package:clean_architecture_todo/app/todo/domain/repository/todo-repository.dart';
 
-class ToDoRepositoryImpl extends ToDoRepository{
-  
-}
+class ToDoRepositoryImpl extends ToDoRepository {
+  @override
+  Future<void> addTodo({required String title, required String description}) {
+    // TODO: implement addTodo
+    throw UnimplementedError();
+  }
 
+  @override
+  Future<void> editTodo(
+      {required String title,
+      required String description,
+      required String itemId}) {
+    // TODO: implement editTodo
+    throw UnimplementedError();
+  } 
+}
